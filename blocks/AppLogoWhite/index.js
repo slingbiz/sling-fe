3e67df2d7318e0fe9deae4084be93e1d5d1bf299
ankur <ankur@sling.biz>
2022-04-25 01:17:52 +0400

    update package

--- blocks/AppLogoWhite/index.js
+++ blocks/AppLogoWhite/index.js
@@ -33,7 +33,7 @@ const AppLogoWhite = () => {
           alt='sling-logo'
         />
       </Hidden>
-      <Box color='white' fontSize={16} fontWeight={500} mt={4} mb={3}>
+      <Box color='white' fontSize={16} fontWeight={500} mt={4} mb={3} onClick={()=> {}}>
         Demo
       </Box>
     </Box>
