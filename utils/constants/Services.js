ee359445e792eda7c1454e11479ce8346d73fa76
Ankur Pata <ankur.patankar@timedoctor.com>
2022-12-14 13:56:51 +0400

    Using core package for the API url

--- utils/constants/Services.js
+++ utils/constants/Services.js
@@ -1,3 +1,6 @@
-export const GET_INIT_PROPS = `${process.env.NEXT_PUBLIC_GET_INIT_PROPS}`;
+import {AppEnums} from 'sling-fe-core';
+
+const {ApiConfig} = AppEnums;
+export const GET_INIT_PROPS = `${process.env.NEXT_PUBLIC_GET_INIT_PROPS || ApiConfig.GET_INIT_PROPS}`;
 export const CLIENT_KEY_SECRET = `${process.env.NEXT_PUBLIC_CLIENT_KEY_SECRET}`;
 export const CLIENT_ID = `${process.env.NEXT_PUBLIC_CLIENT_ID}`;
